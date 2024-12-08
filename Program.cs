c8944098ccae671b3b1bbc2dd8ba9622bac2561e
Jonathan <esguercone96@hotmail.com>
2024-12-08 15:42:00 -0300

    poo desafio atualizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
+Console.WriteLine("\n Nokia");
+Smartphone nokia = new Nokia(numero: "123", modelo: "modelo A", imei: "1111111", memoria: 128);
+Console.WriteLine("\n");
+nokia.Ligar();
+Console.WriteLine("\n");
+nokia.InstalarAplicativo("WhatsApp");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n\n");
+
+Console.WriteLine("\n Iphone");
+Smartphone iphone = new Iphone(numero: "456", modelo: "modelo B", imei: "2222222", memoria: 256);
+Console.WriteLine("\n");
+nokia.Ligar();
+Console.WriteLine("\n");
+nokia.InstalarAplicativo("Telegram");
